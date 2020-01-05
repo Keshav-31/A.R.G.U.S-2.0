ccc39e1ced481edbe23b440bc2dd2e55cf4d7fe0
Divyanshujain2000 <51997241+Divyanshujain2000@users.noreply.github.com>
2020-01-05 14:42:15 +0530

    Add files via upload

--- drive/scripts/sub_joynode_pub_roverdrive.py
+++ drive/scripts/sub_joynode_pub_roverdrive.py
@@ -11,6 +11,75 @@ pwm=[0,0,0,0]
  
 drivex=drive_msg()
 
+def find_pwmtest4(msg):
+    factor = 0.7
+    if(msg.buttons[6]==0):
+        
+        if(msg.axes[2]<1):
+            pwmtotal=interp(msg.axes[2],[1,-1],[0,255])
+        else:
+            pwmtotal=(-1*interp(msg.axes[5],[1,-1],[0,255]))
+
+        #pwmdir=interp(msg.axes[0],[-1,1],[-155,155])
+	    if (msg.buttons[3] == 1):
+	        factor = 1
+	    pwmdir=(msg.axes[0] * pwmtotal * factor * -1)
+        #rospy.loginfo(pwmdir)
+        #rospy.loginfo(pwmtotal)
+        pwm[2]=0
+        pwm[3]=0
+        pwm[0]=0
+        pwm[1]=0
+        if(pwmtotal>=0):
+            leftpwm=pwmtotal+pwmdir
+            rightpwm=pwmtotal-pwmdir
+        else:
+            leftpwm=pwmtotal-pwmdir
+            rightpwm=pwmtotal+pwmdir
+        if(leftpwm<0):
+            pwm[0]=1
+            leftpwm = leftpwm * -1
+        if(rightpwm<0):
+            pwm[1]=1
+            rightpwm = rightpwm * -1
+        if(rightpwm>255):
+            rightpwm=255
+        if(leftpwm>255):
+            leftpwm=255
+        pwm[2] = leftpwm 
+        pwm[3] = rightpwm
+    
+        '''if(pwmtotal<0):
+            pwm[2]=1
+	    pwm[3]=1
+	    pwmtotal=pwmtotal*-1
+        if(pwmdir>=0):
+            pwm[0] = pwmtotal - pwmdir
+            pwm[1]= pwmtotal
+	if(pwmdir<0):
+	    pwm[0]= pwmtotal
+	    pwm[1]= pwmtotal + pwmdir'''
+
+	    if(msg.buttons[4]==1):
+	        pwm[0]=200
+	        pwm[1]=200	
+	        pwm[2]=1
+	        pwm[3]=0	
+	    if(msg.buttons[5]==1):
+	        pwm[0]=200
+	        pwm[1]=200
+	        pwm[2]=0
+	        pwm[3]=1
+    else:
+	    pwm[0]=0
+        pwm[1]=0	
+	    pwm[2]=0
+	    pwm[3]=0
+
+    #rospy.loginfo("left  pwm %f",pwm[0])
+    #rospy.loginfo("right pwm %f",pwm[1])
+    rospy.loginfo(pwm)
+
 def find_pwmtest3(msg):
     '''dir_n = 0 
    
@@ -112,7 +181,7 @@ def find_pwmtest1(msg): #1st case when 2 pwm valus are send for left ans right s
 
 def callback(msg):
     rate = rospy.Rate(100)
-    find_pwmtest3(msg)
+    find_pwmtest4(msg)
     drivex.lpwm = pwm[0] 
     drivex.rpwm = pwm[1]
     drivex.ldir = bool(pwm[2])
